051e329701cc8b7781904a932acb36778f14cd59
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-02 18:58:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.py
@@ -0,0 +1,21 @@
+"""
+* Project Name : Baekjoon Online Judge #11382
+* Program Purpose and Basic Features :
+* - BOJ-Python #11382
+* Program Author : JHKIM
+* Date of original creation : 2024.01.02
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.02	    v1.0	    First Write
+"""
+
+
+def main():
+    A, B, C = map(int, input().split())
+    print(A + B + C)
+
+
+if __name__ == "__main__":
+    main()
