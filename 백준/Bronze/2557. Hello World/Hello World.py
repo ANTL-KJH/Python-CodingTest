be4683c826caf10dc3e0977aa8c71326dae0372f
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-02 12:06:34 +0900

    [Bronze V] Title: Hello World, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.py
@@ -0,0 +1,17 @@
+"""
+* Project Name : Baekjoon Online Judge #2557
+* Program Purpose and Basic Features :
+* - handwritten digits recognition machine learning
+* Program Author : JaeHyun Kim
+* Date of original creation : 2024.01.02
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    		Date		    Version		Content
+* JH KIM			2022.12.02	    v1.0	    First Write
+"""
+def main():
+    print("Hello World!")
+
+if __name__=="__main__":
+    main()
