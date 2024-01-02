1c50112eaba674a77667f8b2539ccaa8396b292c
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-02 17:28:03 +0900

    [Bronze V] Title: A+B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -0,0 +1,18 @@
+"""
+* Project Name : Baekjoon Online Judge #1000
+* Program Purpose and Basic Features :
+* - BOJ-Python #1000
+* Program Author : JaeHyun Kim
+* Date of original creation : 2024.01.02
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    		Date		    Version		Content
+* JH KIM			2024.01.02	    v1.0	    First Write
+"""
+def main():
+    A, B = map(int,input().split())
+    print(A+B)
+
+if __name__=="__main__":
+    main()
