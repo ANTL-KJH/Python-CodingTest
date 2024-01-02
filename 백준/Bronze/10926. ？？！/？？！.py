32fb1063063fe5ef9f1d9682bff349f87f1ce535
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-02 18:33:23 +0900

    [Bronze V] Title: ??!, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.py
@@ -0,0 +1,22 @@
+"""
+* Project Name : Baekjoon Online Judge #10926
+* Program Purpose and Basic Features :
+* - BOJ-Python #10926
+* Program Author : JHKIM
+* Date of original creation : 2024.01.02
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.02	    v1.0	    First Write
+"""
+
+
+def main():
+    inputStr = input()
+    print(inputStr+"??!")
+
+
+
+if __name__ == "__main__":
+    main()
