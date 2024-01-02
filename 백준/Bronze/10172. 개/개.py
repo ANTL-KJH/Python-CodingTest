dc35e8b34276194218db547417b0c7afbee7a780
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-02 19:05:24 +0900

    [Bronze V] Title: 개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,24 @@
+"""
+* Project Name : Baekjoon Online Judge #10172
+* Program Purpose and Basic Features :
+* - BOJ-Python #10172
+* Program Author : JHKIM
+* Date of original creation : 2024.01.02
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.02	    v1.0	    First Write
+"""
+
+
+def main():
+    print("|\\_/|")
+    print("|q p|   /}")
+    print("( 0 )\"\"\"\\")
+    print("|\"^\"`    |")
+    print("||_/=\\\\__|")
+
+
+if __name__ == "__main__":
+    main()
