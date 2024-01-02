26cb6655cd15ccef90280139804a47c0c7f4c67f
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-02 18:35:47 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.py
@@ -0,0 +1,22 @@
+"""
+* Project Name : Baekjoon Online Judge #18108
+* Program Purpose and Basic Features :
+* - BOJ-Python #18108
+* Program Author : JHKIM
+* Date of original creation : 2024.01.02
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.02	    v1.0	    First Write
+"""
+
+
+def main():
+    inputYear = int(input())
+    print(inputYear-543)
+
+
+
+if __name__ == "__main__":
+    main()
