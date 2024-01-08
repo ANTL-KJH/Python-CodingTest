96d867990aa22e9f091769fb8020b6397f598bbf
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-08 14:14:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.py
@@ -0,0 +1,20 @@
+"""
+* Project Name : Baekjoon Online Judge #11654(아스키 코드)
+* Program Purpose and Basic Features :
+* - 입력받은 문자의 아스키코드를 출력하는 프로그램
+* Program Author : JHKIM
+* Date of Original Creation : 2024.01.08
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.08	    v1.0	    First Write
+"""
+
+
+def main():
+    S=input()
+    print(ord(S))
+
+if __name__ == "__main__":
+    main()
