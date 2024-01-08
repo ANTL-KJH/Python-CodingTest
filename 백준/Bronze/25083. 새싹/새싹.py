0a7c203f3f152d85a6a4991ef7a87a8eec78dc59
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-08 16:47:28 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,25 @@
+"""
+* Project Name : Baekjoon Online Judge #25083(새싹)
+* Program Purpose and Basic Features :
+* - 새싹그림을 출력하는 프로그램
+* Program Author : JHKIM
+* Date of Original Creation : 2024.01.08
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.08	    v1.0	    First Write
+"""
+
+
+def main():
+    print("         ,r\'\"7")
+    print("r`-_   ,\'  ,/")
+    print(" \\. \". L_r\'")
+    print("   `~\\/")
+    print("      |")
+    print("      |")
+
+
+if __name__ == "__main__":
+    main()
