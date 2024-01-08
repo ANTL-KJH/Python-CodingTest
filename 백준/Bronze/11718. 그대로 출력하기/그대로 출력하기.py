339a32e1af3999daa4bdbf239bcc468abc8cefda
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-08 16:35:17 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -0,0 +1,22 @@
+"""
+* Project Name : Baekjoon Online Judge #11718(그대로 출력하기)
+* Program Purpose and Basic Features :
+* - 입력받은 문자열을 그대로 출력하는 프로그램
+* Program Author : JHKIM
+* Date of Original Creation : 2024.01.08
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.08	    v1.0	    First Write
+"""
+
+
+def main():
+    while True:
+        try:
+            print(input())
+        except:
+            break
+if __name__ == "__main__":
+    main()
