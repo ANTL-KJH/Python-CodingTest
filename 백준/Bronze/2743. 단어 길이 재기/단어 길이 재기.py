2d77334a5fce882e5b5833d4b23e26b3c77cbd1a
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-08 14:04:58 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.py
@@ -0,0 +1,21 @@
+"""
+* Project Name : Baekjoon Online Judge #2743(단어 길이 재기)
+* Program Purpose and Basic Features :
+* - 알파벳으로만 이루어진 단어를 입력받아, 그 길이를 출력하는 프로그램
+* Program Author : JHKIM
+* Date of original creation : 2024.01.08
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.08	    v1.0	    First Write
+"""
+
+
+def main():
+    S = input()
+    print(len(S))
+
+
+if __name__ == "__main__":
+    main()
