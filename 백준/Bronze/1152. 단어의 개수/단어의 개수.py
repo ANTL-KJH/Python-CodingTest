4c2b69f9058eca7e4c0f7cc83bf784941b58fc06
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-08 15:49:18 +0900

    [Bronze II] Title: 단어의 개수, Time: 56 ms, Memory: 39160 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -0,0 +1,21 @@
+"""
+* Project Name : Baekjoon Online Judge #1152(단어의 개수)
+* Program Purpose and Basic Features :
+* - 주어진 문장에서 단어의 수를 구하는 프로그램
+* Program Author : JHKIM
+* Date of Original Creation : 2024.01.08
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.08	    v1.0	    First Write
+"""
+
+
+def main():
+    S = input().strip().split()
+    print(len(S))
+
+
+if __name__ == "__main__":
+    main()
