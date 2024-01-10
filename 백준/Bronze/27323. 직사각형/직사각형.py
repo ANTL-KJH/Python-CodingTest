14ca21c40481b94d5687aeed6044c4a4836726fe
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-10 11:53:46 +0900

    [Bronze V] Title: 직사각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.py
@@ -0,0 +1,22 @@
+"""
+* Project Name : Baekjoon Online Judge #27323(직사각형)
+* Program Purpose and Basic Features :
+* - 직사각형의 가로와 세로가 주어졌을 때, 넓이를 구하는 프로그램
+* Program Author : JHKIM
+* Date of Original Creation : 2024.01.10
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.10	    v1.0	    First Write
+"""
+
+
+def main():
+    A = int(input())
+    B = int(input())
+    print(A * B)
+
+
+if __name__ == "__main__":
+    main()
