3cf5c50124fd7dd86514419e4fc92cd33e174a4d
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-01-10 16:48:17 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.py
@@ -0,0 +1,21 @@
+"""
+* Project Name : Baekjoon Online Judge #24264(알고리즘 수업 - 알고리즘의 수행 시간 3)
+* Program Purpose and Basic Features :
+* - 코드의 수행 횟수와 최고차항의 차수를 출력하는 프로그램
+* Program Author : JHKIM
+* Date of Original Creation : 2024.01.10
+* ==========================================================================
+* Program History
+* ==========================================================================
+* Author    	Date		    Version		Content
+* JHKIM			2024.01.10	    v1.0	    First Write
+"""
+
+
+def main():
+    n = int(input())
+    print(n*n)
+    print(2)
+
+if __name__ == "__main__":
+    main()
