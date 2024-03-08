5119029663d6f8ee2761a14ad66d7952df032dc7
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-03-08 18:28:10 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -0,0 +1,3 @@
+def solution(arr, intervals):
+    answer = [arr[i] for i in range(intervals[0][0],intervals[0][1]+1)] + [arr[i] for i in range(intervals[1][0],intervals[1][1]+1)]
+    return answer
