0c6cff1bcd672a879a14c602165dfb5983914cfe
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-03-08 17:49:47 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer =[]
+    while True:
+        answer.append(n)
+        if n == 1:
+            break
+        if n % 2 == 0:
+            n//=2
+        else:
+            n = n * 3 + 1
+        
+    return answer
