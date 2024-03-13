dc137b373fb10d5c6a4e1c1978fd405d5181d2c5
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-03-13 14:34:43 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = []
+    for num in range(1, (n // 2) + 1):
+        if n % num == 0:
+            answer.append(num)
+            
+    answer.append(n)
+    answer.sort()
+    return answer
