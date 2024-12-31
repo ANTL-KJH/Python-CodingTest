dbf581e62a5e35e2dd4dd064a684881b9a2080a7
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2024-12-31 16:03:44 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,30 +1,24 @@
-"""
-* Project Name : Baekjoon Online Judge #2563(색종이)
-* Program Purpose and Basic Features :
-* - 색종이를 한 장 또는 여러 장 붙인 후 색종이가 붙은 검은 영역의 넓이를 구하는 프로그램
-* Program Author : JHKIM
-* Date of Original Creation : 2024.01.08
-* ==========================================================================
-* Program History
-* ==========================================================================
-* Author    	Date		    Version		Content
-* JHKIM			2024.01.08	    v1.0	    First Write
-"""
+# 가로 x 세로 = 100 x 100인 빈 리스트 만들기
+paper = []
+for i in range(100) :
+    paper.append([])
+    for j in range(100) :
+        paper[i].append([])
+
+num = int(input())
+
+for input_num in range(num) :
+    coordinate = input().split()
+    x, y = map(int, coordinate)
+    for i in range(x, x+10) :
+        for j in range(y, y+10) :
+            paper[i][j] = [1] # 색종이 이차행렬에 모두 1을 넣기, 문제 : append를 사용할 경우 리스트에 더 많은 1이 계속 저장되어 넓이를 맞게 만들어내지 못한다.
 
 
-def main():
-    mtrx = [list(False for _ in range(100)) for _ in range(100)]
-    N = int(input())
-    for _ in range(N):
-        col, row = map(int, input().split())
-        for i in range(row, row + 10, 1):
-            for j in range(col, col + 10, 1):
-                mtrx[i][j] = True
-    result = 0
-    for i in range(100):
-        result += mtrx[i].count(True)
-    print(result)
+result = 0 # 넓이 출력
+for k in range(100) : 
+    for z in range(100) :
+        result += paper[k][z].count(1) # 1의 개수 = 넓이
 
-
-if __name__ == "__main__":
-    main()
+print(result)
+    
